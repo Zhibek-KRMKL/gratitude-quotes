72cd019309aa13e11e148690843b4fcbbd52983d
Zhibek <thezhibeks@gmail.com>
2024-11-07 07:09:03 +0500

    fix: changed link and added function of button and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
 import { useEffect, useState } from "react";
 import { Button, message, Spin } from "antd";
-import './index.css';
-
-const { Title, Paragraph } = Typography;
 
 function App() {
   const [quote, setQuote] = useState(""); //тут храним цитаты
   const [loading, setLoading] = useState(false); //тут отслеживаем состояние загрузки
 
   useEffect(() => {
-  const fetchQuote = async () => {
-    setLoading(true); //тут вкл индтикатор загрузки
-    try { 
-      const response = await fetch(""); //тут делаем запрос к API с цитатами
-      if (!response.ok) throw new Error('Ошибка при получении цитаты');
+    const fetchQuote = async () => {
+      setLoading(true); //тут вкл индтикатор загрузки
+      try {
+        const response = await fetch("https://zenquotes.io/api/random"); //тут делаем запрос к API с цитатами
+        if (!response.ok) throw new Error("Ошибка при получении цитаты");
+        const data = await response.json();
+        setQuote(data.content); //тут получаем цитату и обновл состояние
+      } catch (error) {
+        message.error(error.message);
+      } finally {
+        setLoading(false); //тут откл индикатор загрузки
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  const handleGenerateQuote = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://zenquotes.io/api/random"); //тут делаем запрос к API с цитатами
+      if (!response.ok) throw new Error("Ошибка при получении цитаты");
       const data = await response.json();
       setQuote(data.content); //тут получаем цитату и обновл состояние
     } catch (error) {
       message.error(error.message);
     } finally {
-      setLoading(false); //тут откл индикатор загрузки
+      setLoading(false);
     }
   };
-  
-    fetchQuote();
-  },[]);
 
   return (
-    <div>
-
+    <div className="quote-container">
+      <h1>Слуйчаная цитата благодарности</h1>
+      {loading ? (
+        <Spin />
+      ) : (
+        <p className="quote-text">
+          {quote || 'Нажмите "Сгенерировать", чтобы получить цитату'}
+        </p>
+      )}
+      <Button type="primaty" onClick={handleGenerateQuote}>
+        Сгенерировать
+      </Button>
     </div>
-  )
-
-
-
+  );
 }
 
 export default App;
