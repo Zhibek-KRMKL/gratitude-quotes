179f3b3b98489fedd60affbf2a002c391b16aea2
Zhibek <thezhibeks@gmail.com>
2024-11-07 06:29:38 +0500

    feat: added a function to receive a request from the API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import axios from "axios";
 
 const { Title, Paragraph } = Typography;
 
-function App() {}
+function App() {
+  const [quote, setQuote] = useState(""); //тут храним цитаты
+  const [loading, setLoading] = useState(false); //тут отслеживаем состояние загрузки
+
+  const fetchQuote = async () => {
+    setLoading(true); //тут вкл индтикатор загрузки
+    try { 
+      const response = await axios.get(""); //тут делаем запрос к API с цитатами
+      setQuote(response.data.content); //тут получаем цитату и обновл состояние
+    } catch (error) {
+      console.error("Error fetching quote", error);
+      setQuote("Не удалось получить цитату. Попробуйте позже.");
+    } finally {
+      setLoading(false); //тут откл индикатор загрузки
+    }
+  }
+}
 
 export default App;
